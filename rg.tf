f9b956d8f112298f4350d4bd516fa1dab021a9ba
Puneet Agrawal <puneet0684@gmail.com>
2025-07-16 22:53:20 +0530

    added new rg

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rgpure"{
  name = "infrarg"
  location = "Central India"
 }
+
+resource "azurerm_resource_group" "rgnewpune"{
+ name = "newrg"
+ location = "south india"
+}
