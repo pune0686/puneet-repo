4605561cdeb0deeff7ed43554c21738b076fa1ea
pune0686 <puneet0686@gmail.com>
2025-07-30 17:06:38 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -8,10 +8,9 @@ terraform {
 }
 
 provider "azurerm" {
-  features{}
-   subscription_id = "25a58ceb-2570-42e2-9d35-00b46979c51a"
+  features {}
+  subscription_id = "25a58ceb-2570-42e2-9d35-00b46979c51a"
 }
-
 resource "azurerm_resource_group" "rgpure"{
  name = "infrarg"
  location = "Central India"
